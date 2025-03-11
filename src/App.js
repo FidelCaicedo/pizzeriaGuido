e517d10424659c3e99b61393df10602276ef9e47
FidelCaicedo <ffcaicedoc@eafit.edu.co>
2025-03-11 16:34:21 -0500

    Commit: Example of using create elements for all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Padre Gino's")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The pepperoni Pizza",
+      description: "Pizza pepperoni my african american friend",
+    }),
+    React.createElement(Pizza, {
+      name: "Pizzample",
+      description: "Example pizza",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
